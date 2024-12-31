a590b4027899b280738bbeb15eb085e0efd7c1e8
etg624 <etg624@gmail.com>
2024-12-31 10:42:47 -0500

    chage keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -26,7 +26,12 @@ vim.keymap.set("n", "<leader>d", '"_d', { desc = 'Delete into " register' })
 vim.keymap.set("v", "<leader>d", '"_d', { desc = 'Delete into " register' })
 
 -- Jump to plugin management file
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/init.lua<CR>", { desc = "Jump to configuration file" })
+vim.keymap.set(
+  "n",
+  "<leader>vkm",
+  "<cmd>e ~/.config/nvim/lua/config/keymaps.lua",
+  { desc = "Jump to configuration file" }
+)
 
 -- Move block
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Block Down" })
