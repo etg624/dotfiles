040ee398108c6a8ae0fb8cc294d8cfecc01a6173
Evan Guirino <etg624@gmail.com>
2025-04-21 10:17:58 -0400

    styling

--- .config/.config/nvim/lua/plugins/cmp.lua
+++ .config/.config/nvim/lua/plugins/cmp.lua
@@ -3,9 +3,10 @@ return {
     "saghen/blink.cmp",
     dependencies = { "giuxtaposition/blink-cmp-copilot" },
     opts = {
+
       keymap = {
         preset = "enter",
-        ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-k>"] = { "select_prev", "select_prev" },
         ["<C-j>"] = { "select_next", "fallback_to_mappings" },
         ["<C-p>"] = {},
         ["<C-n>"] = {},
